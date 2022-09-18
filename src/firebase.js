f6fb7a03bd88b3eabff73863a3891436fc9d3b9f
Kael Paraguassu <kaelparaguassu@hotmail.com>
2022-09-18 17:07:08 -0300

    atualizacao do codigo firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,28 @@
 import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, onValue } from 'firebase/database';
+import { getDatabase, ref, onValue, set } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: 'https://hidra-12609-default-rtdb.firebaseio.com',
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-};
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+//console.log(process.env.NEXT_PUBLIC_FIREBASE_APP_ID);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
-
-export { database, ref, onValue };
+export { auth, database, ref, onValue, set };
+export default app;
