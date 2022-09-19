8c8790cbcb47399f088fed215b7232dea05a6d19
Kael Paraguassu <kaelparaguassu@hotmail.com>
2022-09-19 12:41:43 -0300

    adicao de arquivos para integracao de login/registro com api do suap

---
+++ src/settings.sample.js
@@ -0,0 +1,6 @@
+var CLIENT_ID = 'bpkgsjnHKWprdQeC3huy4x147SVE888wpDyK0Ezg';
+var REDIRECT_URI = 'http://localhost:8888/';
+var SUAP_URL = 'https://suap.ifrn.edu.br';
+var SCOPE = 'identificacao email documentos_pessoais';
+
+export { CLIENT_ID, REDIRECT_URI, SUAP_URL, SCOPE };
